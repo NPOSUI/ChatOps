f76eaa6782fd78c43accba4e2c9823ff4535957a
jianyong.zeng <jianyong.zeng@lianwei.com.cn>
2023-05-19 18:30:49 +0800

    Add authorization demo plugins, let us start coding.

---
+++ botkube/external-plugins/executors/authorization-demo/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/MakeNowJust/heredoc"
+	"github.com/hashicorp/go-plugin"
+	"github.com/kubeshop/botkube/pkg/api"
+	"github.com/kubeshop/botkube/pkg/api/executor"
+	"github.com/kubeshop/botkube/pkg/pluginx"
+	"strings"
+)
+
+// version is set via ldflags by GoReleaser.
+var version = "dev"
+
+const (
+	pluginName  = "echo"
+	description = "Echo is an example Botkube executor plugin used during e2e tests. It's not meant for production usage."
+)
+
+// Config holds executor configuration.
+type Config struct {
+	ChangeResponseToUpperCase *bool `yaml:"changeResponseToUpperCase,omitempty"`
+}
+
+// EchoExecutor implements Botkube executor plugin.
+type EchoExecutor struct{}
+
+var _ executor.Executor = &EchoExecutor{}
+
+// Metadata returns details about Echo plugin.
+func (*EchoExecutor) Metadata(context.Context) (api.MetadataOutput, error) {
+	return api.MetadataOutput{
+		Version:     version,
+		Description: description,
+		JSONSchema:  jsonSchema(),
+	}, nil
+}
+
+// Execute returns a given command as response.
+func (*EchoExecutor) Execute(_ context.Context, in executor.ExecuteInput) (executor.ExecuteOutput, error) {
+	var cfg Config
+	err := pluginx.MergeExecutorConfigs(in.Configs, &cfg)
+	if err != nil {
+		return executor.ExecuteOutput{}, fmt.Errorf("while merging input configuration: %w", err)
+	}
+
+	data := in.Command
+	if strings.Contains(data, "@fail") {
+		return executor.ExecuteOutput{}, errors.New("The @fail label was specified. Failing execution.")
+	}
+
+	if cfg.ChangeResponseToUpperCase != nil && *cfg.ChangeResponseToUpperCase {
+		data = strings.ToUpper(data)
+	}
+
+	return executor.ExecuteOutput{
+		Data: data,
+	}, nil
+}
+
+// Help returns help message
+func (*EchoExecutor) Help(context.Context) (api.Message, error) {
+	return api.Message{}, nil
+}
+
+func main() {
+	executor.Serve(map[string]plugin.Plugin{
+		pluginName: &executor.Plugin{
+			Executor: &EchoExecutor{},
+		},
+	})
+}
+
+func jsonSchema() api.JSONSchema {
+	return api.JSONSchema{
+		Value: heredoc.Docf(`{
+			"$schema": "http://json-schema.org/draft-07/schema#",
+			"title": "botkube/echo",
+			"description": "%s",
+			"type": "object",
+			"properties": {
+				"changeResponseToUpperCase": {
+					"description": "When changeResponseToUpperCase is true, the echoed string will be in upper case",
+					"type": "boolean"
+				}
+			},
+			"required": []
+		}`, description),
+	}
+}
